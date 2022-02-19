e4f21996548d006e51c67fe067e9859ef8c29141
chris-zano <cncs.chris@gmail.com>
2022-02-19 15:30:01 +0000

    Update v1.1.0

--- Scripts/index.js
+++ Scripts/index.js
@@ -38,7 +38,7 @@ function ready() {
             </div>
             <span class="cart-price cart-column">${card.price}</span>
             <div class="cart-quantity cart-column">
-                <input type="number" value="1" class="cart-quantity-input">
+                <input type="number" value="1" class="cart-quantity-input" min="1">
                 <button class="btn btn-danger" type="button">X</button>
             </div>
         
@@ -49,9 +49,11 @@ function ready() {
     }
 
     function removefromCart(e) {
+        var card = e.target.parentElement.parentElement
         if(e.target.classList.contains('btn-danger')) {
-            e.target.parentElement.parentElement.remove();
+            card.remove();
         }
+        removeFromStorage(card)
         updateTotal()
     }
 
@@ -72,7 +74,104 @@ function ready() {
         }
         total = Math.round(total * 100) / 100;
         document.getElementsByClassName('cart-total-price')[0].innerText = 'GHC '+ total;
-
+        cartArray();
     }
 
+    function cartArray() {
+        var cartContainer = document.getElementsByClassName('content-section')[0];
+        var cartItems = cartContainer.getElementsByClassName('cart-items');
+        var total = 0;
+        var cartArray = [];
+        for(var i =0; i < cartItems.length; i++) {
+            var cartItem = cartItems[i];
+            var imgElement = cartItem.getElementsByClassName('cart-item-image')[0];
+            var titleElement = cartItem.getElementsByClassName('cart-item')[0];
+            var priceElement = cartItem.getElementsByClassName('cart-price')[0];
+            var qtyElement = cartItem.getElementsByClassName('cart-quantity-input')[0];
+
+            //get the text from the elements
+            var img = imgElement.src;
+            var title = titleElement.innerText;
+            var price = parseFloat(priceElement.innerText.replace('GHC',''));
+            var quantity = Number(qtyElement.value);
+
+            total = total + (price*quantity);
+            total = Math.round(total * 100) / 100;
+
+            var cartObj = {};
+            cartObj.imgSrc = img;
+            cartObj.title = title;
+            cartObj.price = 'Ghc' + price;
+            cartObj.qty = quantity;
+            cartArray.push(cartObj) ;
+        }
+        cartObj.total = 'total: '+ total;
+        cartArray.push(cartObj.total) ;
+        addToLocalStorage(cartArray)
+    }
+
+    function addToLocalStorage(array) {
+        let localArray = [];
+        localArray.push(array);
+        localStorage.setItem('array',  JSON.stringify(array ));
+        getFromStorage()
+    }
+
+    function getFromStorage() {
+        let storageArray;
+        const arrayLS = localStorage.getItem('array')
+        if(arrayLS === null) {
+            storageArray = [];
+        }
+        else {
+            storageArray = arrayLS;
+        }
+        return storageArray;
+    }
+
+    function arrayOnLoad() {
+        let newArr = getFromStorage();
+        myArr = JSON.parse(newArr)
+        for(i = 0; i < myArr.length-1; i++) {
+            var img = myArr[i].imgSrc;
+            var title = myArr[i].title;
+            var price = myArr[i].price;
+            var qty = parseInt(myArr[i].qty);
+            price = parseFloat(price.replace('Ghc',''))
+            
+            var cartRow =document.createElement('div');
+            cartRow.classList.add('cart-items')
+            cartRow.innerHTML = `
+            <div class="image-title cart-column">
+            <img src="${img}" width="100" height="100" class="cart-item-image">
+            <span class="cart-item">${title}</span>
+            </div>
+            <span class="cart-price cart-column">${price}</span>
+            <div class="cart-quantity cart-column">
+                <input type="number" value=${qty} class="cart-quantity-input" min="1">
+                <button class="btn btn-danger" type="button">X</button>
+            </div>
+        `;
+        cartContent.append(cartRow);
+        }
+        updateTotal
+    }
+    
+
+    function removeFromStorage(card) {
+        let array = getFromStorage()
+
+        var newArray = JSON.parse(array)
+
+        newArray.forEach(function(item,index){
+            if(item.imgSrc === card.imgSrc){
+                newArray.splice(index,1)
+            }
+        })
+
+        localStorage.setItem('array', newArray);
+        
+    }
+
+    arrayOnLoad()
  }
